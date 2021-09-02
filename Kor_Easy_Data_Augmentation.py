e8487bcc1f8872556dc21ea63b04913920f81158
RollsRoyceZung <78933101+RollsRoyceZung@users.noreply.github.com>
2021-09-02 18:22:36 +0900

    Add files via upload

---
+++ Kor_Easy_Data_Augmentation.py
@@ -0,0 +1,186 @@
+#Korean Text Easy Data Augmentation
+import random
+import pickle
+import re
+
+wordnet = {}
+with open("wordnet.pickle", "rb") as f:
+	wordnet = pickle.load(f)
+
+
+# 한글만 남기고 나머지는 삭제
+def get_only_kor(line):
+	parseText= re.compile('/ ^[ㄱ-ㅎㅏ-ㅣ가-힣]*$/').sub('',line)
+
+	return parseText
+
+########################################################################
+# Synonym replacement
+# Replace n words in the sentence with synonyms from wordnet
+# 동의어 교체
+# 문장의 n개 단어를 wordnet의 동의어로 대체
+########################################################################
+def synonym_replacement(words, n):
+	new_words = words.copy()
+	random_word_list = list(set([word for word in words]))
+	random.shuffle(random_word_list)
+	num_replaced = 0
+	for random_word in random_word_list:
+		synonyms = get_synonyms(random_word)
+		if len(synonyms) >= 1:
+			synonym = random.choice(list(synonyms))
+			new_words = [synonym if word == random_word else word for word in new_words]
+			num_replaced += 1
+		if num_replaced >= n:
+			break
+
+	if len(new_words) != 0:
+		sentence = ' '.join(new_words)
+		new_words = sentence.split(" ")
+
+	else:
+		new_words = ""
+
+	return new_words
+
+
+def get_synonyms(word):
+	synomyms = []
+
+	try:
+		for syn in wordnet[word]:
+			for s in syn:
+				synomyms.append(s)
+	except:
+		pass
+
+	return synomyms
+
+########################################################################
+# Random deletion
+# Randomly delete words from the sentence with probability p
+# 무작위 삭제
+# 확률 p로 문장에서 단어를 무작위로 삭제
+########################################################################
+def random_deletion(words, p):
+	if len(words) == 1:
+		return words
+
+	new_words = []
+	for word in words:
+		r = random.uniform(0, 1)
+		if r > p:
+			new_words.append(word)
+
+	if len(new_words) == 0:
+		rand_int = random.randint(0, len(words)-1)
+		return [words[rand_int]]
+
+	return new_words
+
+########################################################################
+# Random swap
+# Randomly swap two words in the sentence n times
+# 랜덤 교체
+# 문장에서 두 단어를 무작위로 n번 교환
+########################################################################
+def random_swap(words, n):
+	new_words = words.copy()
+	for _ in range(n):
+		new_words = swap_word(new_words)
+
+	return new_words
+
+def swap_word(new_words):
+	random_idx_1 = random.randint(0, len(new_words)-1)
+	random_idx_2 = random_idx_1
+	counter = 0
+
+	while random_idx_2 == random_idx_1:
+		random_idx_2 = random.randint(0, len(new_words)-1)
+		counter += 1
+		if counter > 3:
+			return new_words
+
+	new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1]
+	return new_words
+
+########################################################################
+# Random insertion
+# Randomly insert n words into the sentence
+# 랜덤 삽입
+# 문장에 n개의 단어를 무작위로 삽입
+########################################################################
+def random_insertion(words, n):
+	new_words = words.copy()
+	for _ in range(n):
+		add_word(new_words)
+	
+	return new_words
+
+
+def add_word(new_words):
+	synonyms = []
+	counter = 0
+	while len(synonyms) < 1:
+		if len(new_words) >= 1:
+			random_word = new_words[random.randint(0, len(new_words)-1)]
+			synonyms = get_synonyms(random_word)
+			counter += 1
+		else:
+			random_word = ""
+
+		if counter >= 10:
+			return 0
+		
+	random_synonym = synonyms[0]
+	random_idx = random.randint(0, len(new_words)-1)
+	new_words.insert(random_idx, random_synonym)
+
+
+#num+1이 총 개수
+def KOR_EDA(sentence, alpha_sr=0.1, alpha_ri=0.1, alpha_rs=0.1, p_rd=0.1, num_aug=49):
+	sentence = get_only_kor(sentence)
+	words = sentence.split(' ') 
+	words = [word for word in words if word is not ""]
+	num_words = len(words)
+
+	augmented_sentences = []
+	num_new_per_technique = int(num_aug/4) + 1
+
+	n_sr = max(1, int(alpha_sr*num_words))
+	n_ri = max(1, int(alpha_ri*num_words))
+	n_rs = max(1, int(alpha_rs*num_words))
+
+	# SR 동의어 대체
+	for _ in range(num_new_per_technique):
+		a_words = synonym_replacement(words, n_sr)
+		augmented_sentences.append(' '.join(a_words))
+
+	# RI 무작위 삽입
+	for _ in range(num_new_per_technique):
+		a_words = random_insertion(words, n_ri)
+		augmented_sentences.append(' '.join(a_words))
+
+	# RS 랜덤 스왑
+	for _ in range(num_new_per_technique):
+		a_words = random_swap(words, n_rs)
+		augmented_sentences.append(" ".join(a_words))
+
+	# RD 무작위 삭제
+	for _ in range(num_new_per_technique):
+		a_words = random_deletion(words, p_rd)
+		augmented_sentences.append(" ".join(a_words))
+
+	augmented_sentences = [get_only_kor(sentence) for sentence in augmented_sentences]
+	random.shuffle(augmented_sentences)
+
+	if num_aug >= 1:
+		augmented_sentences = augmented_sentences[:num_aug]
+	else:
+		keep_prob = num_aug / len(augmented_sentences)
+		augmented_sentences = [s for s in augmented_sentences if random.uniform(0, 1) < keep_prob]
+
+	augmented_sentences.append(sentence)
+
+	return augmented_sentences
